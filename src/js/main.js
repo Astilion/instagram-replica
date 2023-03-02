40c7627e999b307f75fcc58c491f7323d5749452
Mateusz <mkaminski.contact@gmail.com>
2023-03-02 20:31:09 +0100

    styled post__image and reaction bar + used same API on image--url

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const logo = document.querySelector(".post__bar--logo");
+const postImage = document.querySelector(".post__image--url");
 
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+const PICSUM_URL = "https://picsum.photos/300/400";
 
 const handleLogo = () => {
 	fetch(DOG_URL)
@@ -9,5 +11,10 @@ const handleLogo = () => {
 		.catch(err => console.error(err));
 };
 handleLogo();
-
-const randomUsername = () => {};
+const handleImage = () => {
+	fetch(DOG_URL)
+	.then(res => res.json())
+	.then(data => postImage.setAttribute("src", data.message))
+	.catch(err => console.error(err));
+};
+handleImage()
