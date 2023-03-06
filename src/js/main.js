43336303e7d91024ed0d7f7da5b9d6ed1306801d
Mateusz <mkaminski.contact@gmail.com>
2023-03-06 12:22:52 +0100

    added random number for likes element

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const logo = document.querySelector(".post__bar--logo");
 const postImage = document.querySelector(".post__image--url");
+const likes = document.querySelector('.post__likes-counter--value')
 
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 const PICSUM_URL = "https://picsum.photos/300/400";
@@ -18,3 +19,12 @@ const handleImage = () => {
 	.catch(err => console.error(err));
 };
 handleImage()
+
+const handleNumbers = () => {
+	return Math.floor(Math.random() * 99999) + 1;
+}
+const handleLikes = () => {
+	let likesNumber = handleNumbers();
+	likes.innerHTML = likesNumber
+}
+handleLikes()
